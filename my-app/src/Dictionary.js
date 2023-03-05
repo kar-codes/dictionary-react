dedbf215785ba9338598127db85e4e6450ae5eb2
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-03-05 09:40:08 -0500

    summary

--- my-app/src/Dictionary.js
+++ my-app/src/Dictionary.js
@@ -21,4 +21,4 @@ export default function Dictionary() {
 // what i did here was create an event listener that when the submit button is clicked it calls the function search, then i defined the function search (which receive an event i.e search(event)) inside of the dictionary function before the return
 //Secondly, to get the value that i'm inputting, we need to use a state.
 // So now we create a new state using "let" before the function search(event).
-// Now we have a state called keyword and we want to change the value everytime someone types, so now we add an event listener onChange in the input and assign it a function in this case the functions is handleKeywordChange()
+// Now we have a state called keyword and we want to change the value everytime, someone types, so now we add an event listener onChange in the input and assign it a function in this case the functions is handleKeywordChange()
