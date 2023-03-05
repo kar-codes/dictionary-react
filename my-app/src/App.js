390d2dad952c582f0e1f87bebd94edc139f48bdc
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-03-05 08:53:46 -0500

    Dictionary component created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello world
-      </header>
+      <div className="container">
+        <header className="App-header">
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by Kar and is open-source on Github
+        </footer>
+      </div>
     </div>
   );
 }
