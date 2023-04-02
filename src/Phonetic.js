d6b097222ebb1f27946dfed6457d4da4a9747d3f
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-04-02 13:06:56 -0400

    phonetic functionality added

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { BsFillVolumeUpFill } from "react-icons/bs";
 
-export default function Phonetic({data: {phonetic}}){
+
+export default function Phonetic({data, speechText}){
+    const { speak } = useSpeechSynthesis();
+
+ if (!data) {
+    return null
+ }   
    return (
     <div className="phonetic">
-        {phonetic &&(
-            <p><strong>Phonetic: </strong>{phonetic}</p>
-        )}
-        
+        <button className='btn btn-primary btn-lg speech-btn' 
+        onClick={() => speak({ text: speechText})}>
+        <BsFillVolumeUpFill/>
+        </button>
+        <p><strong>Phonetic: </strong>{data}</p>
     </div>
    )
 }
--- src/Results.js
+++ src/Results.js
@@ -5,6 +5,7 @@ import Phonetic from "./Phonetic";
 export default function Results(props) {
     const meaning = props.results?.meanings?.[1];
     const phonetic = props.results?.phonetic;
+    
 
     if(!props.results) {
         return null;
@@ -13,11 +14,11 @@ export default function Results(props) {
     if (!meaning) {
         return <div className="Results">Word not found</div>;
     }
-
+    console.log(props.results.phonetic);
     return (
         <div className="Results">
             <h2 className="typewriter">{props.results.word}</h2>
-            <Phonetic data={phonetic}/>
+            <Phonetic data={phonetic} speechText={props.results.word}/>
             <Meaning data={meaning}/>
             
         </div>
