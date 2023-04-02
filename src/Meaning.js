16b9de0bf30c9d90c2bc886e63d0467584f4d508
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-04-02 10:46:48 -0400

    synonym implemented

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function Meaning({data: {example, definition, partOfSpeech }}){
+export default function Meaning({data: {example, definition, partOfSpeech, synonyms }}){
     return (
         <div className="meaning">
             <p><strong>Part of speech: </strong>{partOfSpeech}</p>
             <p><strong>Definition: </strong>{definition}.</p>
+            {synonyms && (
+                <p><strong>Synonym: </strong>{synonyms}</p>
+                )}
             {example && (
                 <p><strong>Example: </strong>{example}.</p>
             )}
