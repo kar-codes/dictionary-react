9d2e1c2e168eabb2cb103b488bee207c9f6df9b5
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-04-02 14:12:35 -0400

    styling updated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,33 +4,51 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("sunset");
   const [results, setResults] = useState();
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
-  function search(event) {
-    event.preventDefault();
-    alert(`searching for ${keyword}`);
+  function search(){
     const apiKey = process.env.REACT_APP_DICTIONARY_API_KEY;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
+  function load(){
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
     <div className="Dictionary">
-      <form onSubmit={search}>
+      <form onSubmit={handleSubmit}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <div className="hint">
+      Get definitions, synonyms, translations and more
+      </div>
       <Results results={results} />
     </div>
   );
+} else {
+    load();
+  }
+
+  
 }
 // what i did here was create an event listener that when the submit button is clicked it calls the function search, then i defined the function search (which receive an event i.e search(event)) inside of the dictionary function before the return
 //Secondly, to get the value that i'm inputting, we need to use a state.
